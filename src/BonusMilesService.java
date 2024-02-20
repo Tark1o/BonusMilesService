9431bfa9dd8c05617f68eb3a2042658c8aefb46c
Tarkio <wowsendarpm@gmail.com>
2024-02-20 21:03:02 +0300

    autoformat

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-public int calculate (int price) {
-    int calculate = price / 20;
-    return calculate;
+    public int calculate(int price) {
+        int calculate = price / 20;
+        return calculate;
 
-}
+    }
 
 }
