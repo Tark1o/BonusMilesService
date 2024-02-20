947bfbe38e13b0c7348fa6b4709fb276b931443f
Tarkio <wowsendarpm@gmail.com>
2024-02-20 19:36:51 +0300

    return 500

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
